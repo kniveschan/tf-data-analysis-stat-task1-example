c31edd94c352f041b806980f64c2c15146c30443
kniveschan <44413254+kniveschan@users.noreply.github.com>
2023-04-08 00:48:36 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,4 +12,4 @@ def solution(x: np.array) -> float:
     # Не меняйте название функции и её аргументы
     #print("Дайте пожалуйста еще неделю на решение задачи!")
     #return x.mean() # Ваш ответ
-    return 423 + np.exp(x.mean())
+    return np.exp(x.mean())
